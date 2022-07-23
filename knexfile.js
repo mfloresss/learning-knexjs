42beb978ce29812ebbd55209c1de76cfd0effb1a
Michael Flores <mich4el.flores@gmail.com>
2022-07-22 23:51:30 -0300

    Usage .env to connect DB and Add gitignore

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 // Update with your config settings.
 module.exports = {
   development: {
     client: "mysql2",
     connection: {
       database: "knex",
-      user: "micha",
-      password: "micha1?",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
